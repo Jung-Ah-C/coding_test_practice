73778c10e95cf465cc6189a27c8b1d86861c643d
Jung-Ah-C <wjddk718@gmail.com>
2022-06-21 17:23:22 +0900

    문장 속 단어 문제

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,22 +2,25 @@ import java.util.Scanner;
 
 public class Main {
     public String solution(String str) {
-        String result = "";
-        for(char x : str.toCharArray()) {
-            if(Character.isUpperCase(x)) {
-                result+=Character.toLowerCase(x);
-            } else {
-                result+=Character.toUpperCase(x);
+        String answer = "";
+        String[] strArr = str.split(" ");
+        int min = Integer.MIN_VALUE;
+
+        for(String s : strArr) {
+            int length = s.length();
+            if(length > min) {
+                min = length;
+                answer = s;
             }
         }
-        return result;
+        return answer;
     }
 
 
     public static void main(String[] args) {
         Main T = new Main();
         Scanner kb = new Scanner(System.in);
-        String str = kb.next();
+        String str = kb.nextLine();
         System.out.println(T.solution(str));
     }
 }
