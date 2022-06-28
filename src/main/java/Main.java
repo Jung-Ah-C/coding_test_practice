20be099115382b2c6425a19b7c5582be7c15e73a
Jung-Ah-C <wjddk718@gmail.com>
2022-06-28 14:44:03 +0900

    단어 뒤집기 (StringBuilder, reverse)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,14 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-    public String solution(String str) {
-        String answer = "";
-        String[] strArr = str.split(" ");
-        int min = Integer.MIN_VALUE;
-
-        for(String s : strArr) {
-            int length = s.length();
-            if(length > min) {
-                min = length;
-                answer = s;
-            }
+    public ArrayList<String> solution(int n, String[] str) {
+        ArrayList<String> answer = new ArrayList<>();
+        for(String x : str) {
+            String tmp = new StringBuilder(x).reverse().toString();
+            answer.add(tmp);
         }
+
         return answer;
     }
 
@@ -20,7 +16,13 @@ public class Main {
     public static void main(String[] args) {
         Main T = new Main();
         Scanner kb = new Scanner(System.in);
-        String str = kb.nextLine();
-        System.out.println(T.solution(str));
+        int n = kb.nextInt();
+        String[] str = new String[n];
+        for(int i=0; i<n; i++) {
+            str[i] = kb.next();
+        }
+        for(String x : T.solution(n, str)) {
+            System.out.println(x);
+        }
     }
 }
